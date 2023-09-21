014c52ff21ac022422b51d19c0e8148978065e91
ar1619 <andrianirina.rakotoharisoa19@imperial.ac.uk>
2023-09-21 17:25:45 +0100

    implement first test case

--- classification_bayes.py
+++ classification_bayes.py
@@ -3,4 +3,7 @@ import torch.nn as nn
 from network_elements import Conv2dB, LinearB, FlattenLayer, ModuleProb
 
 class TestNet(ModuleProb):
-    
+    def __init__(self, inputs, outputs, priors, activation_type = 'softplus'):
+        super(TestNet, self).__init__()
+
+        
